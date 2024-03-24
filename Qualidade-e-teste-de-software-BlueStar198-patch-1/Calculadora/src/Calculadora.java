72f62a63f7a9f30483e16f1a093ce54dce71c69a
BlueStar198 <41968462+BlueStar198@users.noreply.github.com>
2024-03-24 15:48:07 -0300

    Adicionar javadoc

---
+++ Qualidade-e-teste-de-software-BlueStar198-patch-1/Calculadora/src/Calculadora.java
@@ -0,0 +1,48 @@
+
+/*
+ * @Author KaykyOda
+ * @Version 1.2
+ * @Since Release 1.2 do programa Calculadora
+ */
+
+ public class Calculadora {
+    /**
+     * Calcula a soma de dois inteiros.
+     * @param a O primeiro inteiro.
+     * @param b O segundo inteiro.
+     * @return A soma de a e b.
+     */
+    public int Somar(int a, int b){ // Calculo de Soma
+        return a + b;
+    }
+    
+    /**
+     * Calcula a subtração de dois inteiros.
+     * @param a O primeiro inteiro.
+     * @param b O segundo inteiro.
+     * @return A subtração de a por b.
+     */
+    public int Subtrair(int a, int b){ // Calculo de Subtração
+        return a - b;
+    }
+    
+    /**
+     * Calcula a multiplicação de dois inteiros.
+     * @param a O primeiro inteiro.
+     * @param b O segundo inteiro.
+     * @return A multiplicação de a por b.
+     */
+    public int Multiplicar(int a, int b){ // Calculo de Multiplicação
+        return a * b;
+    }
+    
+    /**
+     * Calcula a divisão de dois inteiros.
+     * @param a O dividendo.
+     * @param b O divisor.
+     * @return A Divisão de a por b.
+     */
+    public double Dividir(int a, int b){ // Calculo de Divisão
+        return a / b;
+    }
+}
